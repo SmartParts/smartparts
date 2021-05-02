90dd3959b29b20e027d94948739cd3656f0d9f81
Sahil Batra <sahilbatra1211@gmail.com>
2021-05-02 19:56:58 +0530

    added navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,69 @@
+import React, { Fragment,useState } from 'react';
+import {Menu,MenuItem,AppBar,Typography,Toolbar,Button,IconButton} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import {Link} from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+
+// const useStyles = makeStyles((theme)=>({
+//     root:{
+//         width: "100vw",
+//         height:"100vh",
+//         backgroundColor: theme.palette.grey[300],
+//         paddingTop: theme.spacing(5),
+//     }
+// }))
+
+const useStyles = makeStyles((theme) =>({
+    sectionDesktop: {
+        display: "none",
+        [theme.breakpoints.up("md")] :{
+            display:"flex"
+        }
+    },
+    hideIcon:{
+        [theme.breakpoints.up("md")]:{
+            display:"none"
+        }
+    }
+}))
+
+export default function Navbar() {
+    const [mobileMenuAnchorEl, setMobileMenuAnchorEl] = useState(null);
+    const classes = useStyles();
+    const isMobileMenuOpen = Boolean(mobileMenuAnchorEl);
+
+    const openMobileMenu = (event) =>{
+        setMobileMenuAnchorEl(event.currentTarget)
+    }
+    const closeMobileMenu = () =>{
+        setMobileMenuAnchorEl(null);
+    }
+
+    const mobileMenu =(
+        <Menu id="mobile-menu" anchorEl={mobileMenuAnchorEl} keepMounted open={isMobileMenuOpen} >
+            <MenuItem>Home</MenuItem>
+            <MenuItem>Contact</MenuItem>
+            <MenuItem>About</MenuItem>
+        </Menu>
+    );
+    return (
+        <>
+        <AppBar>
+            <Toolbar>
+                <Typography variant="h5" style={{flexGrow:1}}>
+                       OneLogView
+                </Typography>
+                <div className={classes.sectionDesktop}>
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component={Link} to="/about">About</Button>
+                    <Button color="inherit">Contact</Button>
+                </div>
+                <IconButton className={classes.hideIcon} color="inherit" onClick={openMobileMenu}>
+                    <MenuIcon/>
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+        {mobileMenu}
+        </>
+    )
+}
---
+++ src/Components/Others.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Box,Typography} from "@material-ui/core";
+
+export default function Home() {
+    return (
+        <Box py={20}>
+            <Typography variant="h1">Others</Typography>
+        </Box>
+    )
+}
