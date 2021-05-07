480e68593d4e6b1dcc0e133c78e5a5ae1c36da70
Sahil Batra <sahilbatra1211@gmail.com>
2021-05-08 02:01:19 +0530

    table basic structure improved

--- src/Components/Overview.tsx
+++ src/Components/Overview.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {Table,TableContainer,Paper,TableHead,TableRow,TableCell, TableBody} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
+import {makeStyles,withStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
     table: {
       minWidth: 650,
     },
+    tableCell:{
+        border: "1px solid rgba(224, 224, 224, 1)",
+        padding:"10px 0px"
+    },
+    tableSubHeader:{
+        border: "1px solid rgba(224, 224, 224, 1)",
+        padding:"0px"
+    }
   });
+
 
 const Overview = ()=> {
     const classes =useStyles();
@@ -16,13 +25,21 @@ const Overview = ()=> {
              <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell align="center">Tracing</TableCell>
-                        <TableCell align="center">Backend</TableCell>
-                        <TableCell align="center">RequestInfo</TableCell>
+                        <TableCell style={{width: "20%" }} className={classes.tableCell} align="center">Tracing</TableCell>
+                        <TableCell style={{width: "20%" }} className={classes.tableCell} align="center">Backend</TableCell>
+                        <TableCell style={{width: "20%" }} align="center"></TableCell>
+                        <TableCell style={{width: "20%" }}  align="center">Backend</TableCell>
+                        <TableCell style={{width: "20%" }} align="center"></TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-
+                    <TableRow>
+                        <TableCell style={{width: "20%"}} className={classes.tableCell} align="center">Tracing</TableCell>
+                        <TableCell style={{width: "20%"}} className={classes.tableCell} align="center">Tracing</TableCell>
+                        <TableCell style={{width: "20%"}} className={classes.tableCell} align="center">Tracing</TableCell>
+                        <TableCell style={{width: "20%"}} className={classes.tableCell} align="center">Tracing</TableCell>
+                        <TableCell style={{width: "20%"}} className={classes.tableCell} align="center">Tracing</TableCell>
+                    </TableRow>
                 </TableBody>
              </Table>
         </TableContainer>
